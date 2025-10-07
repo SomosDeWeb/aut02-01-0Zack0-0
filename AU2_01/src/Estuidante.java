439f9f585e6979d2067ef4a9e949787c8b8a5c04
Arturo Caamaño <zaacckfvck@gmail.com>
2025-10-07 21:52:18 +0100

    objeto-estudiante

---
+++ AU2_01/src/Estuidante.java
@@ -0,0 +1,42 @@
+public class Estuidante {
+    private String nombre;
+    private int edad;
+    private String  curso;
+    private double nota;
+}
+
+public Estuidante(String nombre, int edad, String curso, double nota) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.curso = curso;
+    this.nota = nota;
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public int getEdad() {
+    return edad;
+}
+
+public void setEdad(int edad) {
+    this.edad = edad;
+}
+
+public double getNota() {
+    return nota;
+}
+@Override
+public String toString() {
+    return "Estuidante{" +
+            "nombre='" + nombre + '\'' +
+            ", edad=" + edad +
+            ", curso='" + curso + '\'' +
+            ", nota=" + nota +
+            '}';
+}
